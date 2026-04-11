bacfa2562191cb8849a2edd701729a4f7b53b710
Max Zimmermann <115488694+Maxzimmerman@users.noreply.github.com>
2026-04-12 05:17:25 +0800

    codecrafters submit [skip ci]

--- lib/main.ex
+++ lib/main.ex
@@ -23,9 +23,21 @@ defmodule Server do
     # # Since the tester restarts your program quite often, setting SO_REUSEADDR
     # # ensures that we don't run into 'Address already in use' errors
     {:ok, socket} = :gen_tcp.listen(6379, [:binary, active: false, reuseaddr: true])
-    handle_ping(socket)
+    get_clients(socket)
   end
 
+  def get_clients(socket) do
+    case :gen_tcp.accept(socket) do
+      {:ok, client} ->
+        :gen_tcp.send(client, "+PONG\r\n")
+        get_clients(socket)
+      {:error, reason} ->
+        Logger.error("Error accepting connection: #{reason}")
+    end
+
+  end
+
+  @spec handle_ping(any()) :: any()
   def handle_ping(socket) do
     Task.async(fn ->
       with {:ok, client} <- :gen_tcp.accept(socket) do
