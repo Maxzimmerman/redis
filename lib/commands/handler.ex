8e43468e50877629f2784c45cfb0155a06f307e5
Max Zimmermann <115488694+Maxzimmerman@users.noreply.github.com>
2026-04-12 15:08:39 +0800

    codecrafters submit [skip ci]

--- lib/commands/handler.ex
+++ lib/commands/handler.ex
@@ -19,7 +19,7 @@ defmodule Commands.Handler do
   end
 
   defp find_command({:ok, data}) do
-    IO.inspect("Received data: #{String.split(data, "n$") |> List.first()}")
+    IO.inspect("Received data: #{String.split(data, "n$")}")
     Map.get(@commands, String.trim(data))
   end
 
