2ea6112d9c841c6b1f0a880e0201b63ee51c4877
Max Zimmermann <115488694+Maxzimmerman@users.noreply.github.com>
2026-04-12 15:34:39 +0800

    codecrafters submit [skip ci]

--- lib/commands/ping.ex
+++ lib/commands/ping.ex
@@ -2,9 +2,7 @@ defmodule Commands.Ping do
   @behaviour Commands.Behaviour
   require Logger
 
-  @spec handle_client_async(any(), port() | {:"$inet", atom(), any()}, any()) :: :ok
   def handle_client_async(socket, client, args) do
-    {:ok, client} = :gen_tcp.accept(socket)
     Logger.info(client: client, args: args, message: "Received ECHO command")
     case :gen_tcp.recv(client, 0) do
       {:ok, _data} ->
