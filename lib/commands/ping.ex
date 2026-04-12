7da01912ac3250bd399066e33a5e68a2f1185acd
Max Zimmermann <115488694+Maxzimmerman@users.noreply.github.com>
2026-04-12 15:22:46 +0800

    codecrafters submit [skip ci]

--- lib/commands/ping.ex
+++ lib/commands/ping.ex
@@ -5,10 +5,12 @@ defmodule Commands.Ping do
   def handle_client_async(client, args) do
     case :gen_tcp.recv(client, 0) do
       {:ok, _data} ->
+        IO.puts("Received PING command with args: #{inspect(args)}")
         :gen_tcp.send(client, "+PONG\r\n")
         handle_client_async(client, args)
 
       {:error, :closed} ->
+        IO.inspect("Erorr")
         :gen_tcp.close(client)
     end
   end
