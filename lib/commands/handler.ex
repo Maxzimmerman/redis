06a12aec204c261873ce9ef70c75b7da2fc53180
Max Zimmermann <115488694+Maxzimmerman@users.noreply.github.com>
2026-04-12 15:33:38 +0800

    codecrafters submit [skip ci]

--- lib/commands/handler.ex
+++ lib/commands/handler.ex
@@ -10,7 +10,7 @@ defmodule Commands.Handler do
     case :gen_tcp.accept(socket) do
       {:ok, client} ->
         {command, args} = find_command(:gen_tcp.recv(client, 0))
-        Task.start(fn -> command.handle_client_async(client, args) end)
+        Task.start(fn -> command.handle_client_async(socket, client, args) end)
         handle_connections_async(socket)
 
       {:error, reason} ->
