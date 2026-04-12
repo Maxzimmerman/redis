1c67c9ba4f0ad8bdd936271806f3349d4e686ed0
Max Zimmermann <115488694+Maxzimmerman@users.noreply.github.com>
2026-04-12 15:32:48 +0800

    codecrafters submit [skip ci]

--- lib/commands/echo.ex
+++ lib/commands/echo.ex
@@ -2,14 +2,14 @@ defmodule Commands.Echo do
   @behaviour Commands.Behaviour
   require Logger
 
-  def handle_client_async(client, args) do
+  def handle_client_async(socket, client, args) do
     Logger.info(client: client, args: args, message: "Received ECHO command")
     message = List.first(args)
     :gen_tcp.send(client, "$#{byte_size(message)}\r\n#{message}\r\n")
 
     case :gen_tcp.recv(client, 0) do
       {:ok, _data} ->
-        handle_client_async(client, args)
+        handle_client_async(socket, client, args)
       {:error, :closed} -> :gen_tcp.close(client)
        _ -> Logger.error("Error receiving data")
       end
--- lib/commands/ping.ex
+++ lib/commands/ping.ex
@@ -2,12 +2,14 @@ defmodule Commands.Ping do
   @behaviour Commands.Behaviour
   require Logger
 
-  def handle_client_async(client, args) do
+  @spec handle_client_async(any(), port() | {:"$inet", atom(), any()}, any()) :: :ok
+  def handle_client_async(socket, client, args) do
+    {:ok, client} = :gen_tcp.accept(socket)
     Logger.info(client: client, args: args, message: "Received ECHO command")
     case :gen_tcp.recv(client, 0) do
       {:ok, _data} ->
         :gen_tcp.send(client, "+PONG\r\n")
-        handle_client_async(client, args)
+        handle_client_async(socket, client, args)
 
       {:error, :closed} ->
         :gen_tcp.close(client)
