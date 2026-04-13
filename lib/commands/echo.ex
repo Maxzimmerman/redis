89a54b2e8e2eaddc1b1ca2eeedcb8d50735329e2
Max Zimmermann <115488694+Maxzimmerman@users.noreply.github.com>
2026-04-14 00:23:50 +0800

    codecrafters submit [skip ci]

--- lib/commands/echo.ex
+++ lib/commands/echo.ex
@@ -4,14 +4,7 @@ defmodule Commands.Echo do
 
   def execute(client, message) do
     Logger.info(client: client, message: message)
-    case :gen_tcp.recv(client, 0) do
-      {:ok, _data} ->
-        :gen_tcp.send(client, "$#{byte_size(message)}\r\n#{message}\r\n")
-        execute(client, message)
-
-      {:error, :closed} ->
-        :gen_tcp.close(client)
-
-    end
+    msg = List.first(message)
+    :gen_tcp.send(client, "$#{byte_size(msg)}\r\n#{msg}\r\n")
   end
 end
