99007435f64161466febbccb180cc0bee9cc0775
Max Zimmermann <115488694+Maxzimmerman@users.noreply.github.com>
2026-04-14 00:05:20 +0800

    codecrafters submit [skip ci]

--- lib/commands/handler.ex
+++ lib/commands/handler.ex
@@ -1,11 +1,6 @@
 defmodule Commands.Handler do
   require Logger
 
-  @commands %{
-    "PING" => Commands.Ping,
-    "ECHO" => Commands.Echo
-  }
-
   def handle_connections_async(socket) do
     Commands.Ping.handle_connections_async(socket)
     Commands.Echo.handle_connections_async(socket)
