9a63226009e42fed018eb9139524dfbfe83c9b79
Max Zimmermann <115488694+Maxzimmerman@users.noreply.github.com>
2026-04-14 00:08:11 +0800

    codecrafters submit [skip ci]

--- lib/commands/handler.ex
+++ lib/commands/handler.ex
@@ -2,7 +2,7 @@ defmodule Commands.Handler do
   require Logger
 
   def handle_connections_async(socket) do
-    Task.async(fn -> Commands.Echo.handle_connections_async(socket) end)
-    Task.async(fn -> Commands.Ping.handle_connections_async(socket) end)
+    Task.async(fn -> Commands.Echo.handle_connections_async(socket) end) |> Task.await()
+    Task.async(fn -> Commands.Ping.handle_connections_async(socket) end) |> Task.await()
   end
 end
