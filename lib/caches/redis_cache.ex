e94b8577b3f3110d8b0e92b9be87c1081f2315ea
Max Zimmermann <115488694+Maxzimmerman@users.noreply.github.com>
2026-04-17 00:13:18 +0800

    codecrafters submit [skip ci]

---
+++ lib/caches/redis_cache.ex
@@ -0,0 +1,3 @@
+defmodule RedisCache do
+
+end
--- lib/commands/set.ex
+++ lib/commands/set.ex
@@ -2,11 +2,18 @@ defmodule Commands.Set do
   @behaviour Commands.Behaviour
   require Logger
 
-  def execute(client, message, key_values) do
+  def execute(client, message, key_values) when length(message) == 2 do
     Logger.info(client: client, message: message)
     IO.inspect(message, label: "SET command message")
     # For simplicity, we just acknowledge the SET command without actual storage
     :gen_tcp.send(client, "+OK\r\n")
     Map.put(key_values, List.first(message), List.last(message))
+  end
+
+  def execute(client, message, key_values) do
+    Logger.error(client: client, message: "Invalid SET command format")
+    :gen_tcp.send(client, "+OK\r\n")
+    [key, value | _] = message
+     Map.put(key_values, key, value)
   end
 end
