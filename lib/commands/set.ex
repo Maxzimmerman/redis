257e946a4d02427d968771098ccea43ca16ab798
Max Zimmermann <115488694+Maxzimmerman@users.noreply.github.com>
2026-04-16 23:17:36 +0800

    codecrafters submit [skip ci]

--- lib/commands/set.ex
+++ lib/commands/set.ex
@@ -4,6 +4,7 @@ defmodule Commands.Set do
 
   def execute(client, message, key_values) do
     Logger.info(client: client, message: message)
+    IO.inspect(message, label: "SET command message")
     # For simplicity, we just acknowledge the SET command without actual storage
     :gen_tcp.send(client, "+OK\r\n")
     Map.put(key_values, List.first(message), List.last(message))
