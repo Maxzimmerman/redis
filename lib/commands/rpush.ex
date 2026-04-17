e566bd0512bc554b5e2f83fb380f47a396166a2c
Max Zimmermann <115488694+Maxzimmerman@users.noreply.github.com>
2026-04-17 18:23:03 +0800

    codecrafters submit [skip ci]

--- lib/commands/rpush.ex
+++ lib/commands/rpush.ex
@@ -3,7 +3,7 @@ defmodule Commands.RPush do
   require Logger
 
   def execute(client, message, cache_pid) do
-    Logger.info(client: client, message: message)
+    Logger.info(client: client, message: message, command: "RPUSH")
 
     :gen_tcp.send(client, "+OK\r\n")
     [key | values] = message
