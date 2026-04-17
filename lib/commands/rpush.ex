075a38ad6b83fc9665a73932070f1b3b6183d7e3
Max Zimmermann <115488694+Maxzimmerman@users.noreply.github.com>
2026-04-17 18:33:49 +0800

    codecrafters submit [skip ci]

--- lib/commands/rpush.ex
+++ lib/commands/rpush.ex
@@ -11,12 +11,12 @@ defmodule Commands.RPush do
       nil ->
         RedisCache.set(cache_pid, %{key => values})
         IO.inspect(values, label: "Values being pushed to new list")
-        :gen_tcp.send(client, "+#{length(values)}")
+        :gen_tcp.send(client, "+#{length(values)}\r\n")
       existing_values when is_list(existing_values) ->
         new_values = existing_values ++ values
         IO.inspect(new_values, label: "Values being pushed to list #{inspect(existing_values)}")
         RedisCache.set(cache_pid, %{key => new_values})
-        :gen_tcp.send(client, "+#{length(new_values)}")
+        :gen_tcp.send(client, "+#{length(new_values)}\r\n")
     end
   end
 end
