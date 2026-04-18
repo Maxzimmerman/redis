b452094026ff9335bbc0930f34f8b815be405993
Max Zimmermann <115488694+Maxzimmerman@users.noreply.github.com>
2026-04-18 17:10:38 +0800

    codecrafters submit [skip ci]

--- lib/commands/lrange.ex
+++ lib/commands/lrange.ex
@@ -6,24 +6,29 @@ defmodule Commands.LRange do
     Logger.info(client: client, message: message, command: "LRange")
 
     [key | [start_index, end_index]] = message
-    start_index = String.to_integer(start_index)
-    end_index = String.to_integer(end_index)
     length = RedisCache.get_length(cache_pid, key)
+    start_index = build_index(String.to_integer(start_index), length)
+    end_index = build_index(String.to_integer(end_index), length)
 
-    if start_index < 0 and end_index < 0 do
-      start_index = length - start_index * -1
-      end_index = length - end_index * -1
-
-      case RedisCache.get_range(
-             cache_pid,
-             key,
-             start_index,
-             end_index
-           ) do
-        nil -> :gen_tcp.send(client, "*0\r\n")
-        elements when is_list(elements) -> :gen_tcp.send(client, build_response(elements))
-      end
+    case RedisCache.get_range(
+           cache_pid,
+           key,
+           start_index,
+           end_index
+         ) do
+      nil -> :gen_tcp.send(client, "*0\r\n")
+      elements when is_list(elements) -> :gen_tcp.send(client, build_response(elements))
     end
+  end
+
+  defp build_index(index, length) do
+    new_index =
+      case index do
+        i when i < 0 -> length + index
+        i when i >= 0 -> i
+      end
+
+    new_index
   end
 
   defp build_response(elements) do
