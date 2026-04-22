c67199324ef92660c9dcad81aa3adee2ab7e308f
Max Zimmermann <115488694+Maxzimmerman@users.noreply.github.com>
2026-04-23 00:05:48 +0800

    codecrafters submit [skip ci]

--- lib/commands/blpop.ex
+++ lib/commands/blpop.ex
@@ -5,12 +5,14 @@ defmodule Commands.BLPop do
 
   alias Events.Event
 
+  @impl true
   def execute(client, message, _key_values) do
     Logger.info(client: client, message: message)
     msg = List.first(message)
     :gen_tcp.send(client, "$#{byte_size(msg)}\r\n#{msg}\r\n")
   end
 
+  @impl true
   def handle_event(%Event{type: "element_added"} = event) do
     Logger.info("Received event for list #{event.payload.list_key} with new element: #{event.payload.element}")
     :ok
