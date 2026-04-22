99bba7743e52434ee6c1ea43a14f90791f7bbf3b
Max Zimmermann <115488694+Maxzimmerman@users.noreply.github.com>
2026-04-23 00:28:02 +0800

    codecrafters submit [skip ci]

--- lib/caches/redis_cache.ex
+++ lib/caches/redis_cache.ex
@@ -38,33 +38,40 @@ defmodule RedisCache do
 
   @impl true
   def handle_call({:update, key_value_pair}, _from, state) do
-    [{key, value}] = Map.to_list(key_value_pair)
-    new_state = Map.put(state, key, Map.get(state, key) ++ value)
+    [{key, values}] = Map.to_list(key_value_pair)
+    new_list = :queue.join(Map.get(state, key), :queue.from_list(values))
+    new_state = Map.put(state, key, new_list)
     {:reply, :ok, new_state}
   end
 
   @impl true
   def handle_call({:update_prepend, key_value_pair}, _from, state) do
     [{key, value}] = Map.to_list(key_value_pair)
-    new_values = Enum.reverse(value)
-    new_state = Map.put(state, key, new_values ++ Map.get(state, key))
+    new_list = :queue.join(:queue.from_list(value), Map.get(state, key))
+    new_state = Map.put(state, key, new_list)
     {:reply, :ok, new_state}
   end
 
   @impl true
   def handle_call({:pop_list_element, key}, _from, state) do
-    [item_to_remove | updated_list] = state[key]
-    new_state = Map.put(state, key, updated_list)
-    {:reply, item_to_remove, new_state}
+    case :queue.out(state[key]) do
+      {:empty, _} ->
+        {:reply, nil, state}
+      {{:value, item_to_remove}, updated_queue} ->
+        new_state = Map.put(state, key, updated_queue)
+        {:reply, item_to_remove, new_state}
+    end
   end
 
   @impl true
   def handle_call({:pop_list_elements, key, number_to_remove}, _from, state) do
-    IO.inspect(number_to_remove, label: "Number to remove")
-    to_remove = Enum.take(state[key], String.to_integer(number_to_remove))
-    updated_list = Enum.drop(state[key], String.to_integer(number_to_remove))
-    new_state = Map.put(state, key, updated_list)
-    {:reply, to_remove, new_state}
+    case :queue.out_r(state[key]) do
+      {:empty, _} ->
+        {:reply, nil, state}
+      {{:value, item_to_remove}, updated_queue} ->
+        new_state = Map.put(state, key, updated_queue)
+        {:reply, item_to_remove, new_state}
+    end
   end
 
   @doc """
