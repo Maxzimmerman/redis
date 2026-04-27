620bdf8dcf79fe0131b41b4be8ee9e4253185455
Max Zimmermann <115488694+Maxzimmerman@users.noreply.github.com>
2026-04-27 22:27:17 +0800

    codecrafters submit [skip ci]

--- mix.exs
+++ mix.exs
@@ -27,7 +27,9 @@ defmodule App.MixProject do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
       {:typed_struct, "~> 0.3.0"},
-      {:jason, "~> 1.2"}
+      {:jason, "~> 1.2"},
+      {:ecto_sql, "~> 3.0"},
+      {:postgrex, ">= 0.0.0"}
     ]
   end
 end
