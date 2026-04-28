40327087e919403c92c95f36e5b9466fa255f370
Max Zimmermann <115488694+Maxzimmerman@users.noreply.github.com>
2026-04-29 00:41:23 +0800

    codecrafters submit [skip ci]

--- lib/commands/blpop.ex
+++ lib/commands/blpop.ex
@@ -30,8 +30,8 @@ defmodule Commands.BLPop do
         :gen_tcp.send(event.payload.client, "$*\r\n")
 
       element ->
+                IO.inspect(          "$*2\r\n$#{byte_size(event.payload.list_key)}\r\n#{event.payload.list_key}\r\n$#{byte_size(element)}\r\n#{element}\r\n")
         IO.inspect("Popped element '#{element}' from list #{event.payload.list_key}, sending to client.")
-        IO.inspect(          "$*2\r\n$#{byte_size(event.payload.list_key)}\r\n#{event.payload.list_key}\r\n$#{byte_size(element)}\r\n#{element}\r\n")
         :gen_tcp.send(
           event.payload.client,
           "$*2\r\n$#{byte_size(event.payload.list_key)}\r\n#{event.payload.list_key}\r\n$#{byte_size(element)}\r\n#{element}\r\n"
