630786ef36fe6a6a76720f8cf7b94822d813e7d2
Max Zimmermann <115488694+Maxzimmerman@users.noreply.github.com>
2026-04-30 18:33:35 +0800

    codecrafters submit [skip ci]

--- lib/commands/xadd.ex
+++ lib/commands/xadd.ex
@@ -8,6 +8,13 @@ defmodule Commands.XADD do
   def execute(client, [key, id | fields], cache_pid) do
     stream = %Stream{id: id}
 
+    fields =
+      Enum.reduce(fields, fn [first, second | rest] ->
+        %{first => second}
+      end)
+
+    IO.inspect(fields)
+
     RedisCache.set(cache_pid, %{key => stream})
 
     :gen_tcp.send(client, "$#{byte_size(id)}\r\n#{id}\r\n")
