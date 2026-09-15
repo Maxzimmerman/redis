fa9c4d9edfe032ef465779f388ae6a20f96413f4
Max Zimmermann <115488694+Maxzimmerman@users.noreply.github.com>
2026-09-15 22:15:39 +0800

    codecrafters submit [skip ci]

--- lib/commands/xadd.ex
+++ lib/commands/xadd.ex
@@ -6,7 +6,15 @@ defmodule Commands.XADD do
 
   @impl true
   def execute(client, [key, id | fields], cache_pid) do
-    validate_key(id)
+    # TODO add function on cash to return most recent entry
+    #
+
+    prev_id =
+      RedisCache.get_most_recent()
+      |> Map.keys()
+      |> Enum.take(0)
+
+    id_higher_than_prev?(id, prev_id)
 
     updated_fields =
       fields
@@ -22,8 +30,19 @@ defmodule Commands.XADD do
     :gen_tcp.send(client, "$#{byte_size(id)}\r\n#{id}\r\n")
   end
 
-  defp validate_key(id) do
-    IO.inspect("#{System.system_time(:millisecond)}-#{id}", label: "INPUT")
+  defp id_higher_than_prev?(full_current_id, prev_full_id) do
+    current_id = convert_to_id(full_current_id)
+    prev_id = convert_to_id(prev_full_id)
+    String.to_float(current_id) > String.to_float(prev_id)
+  end
+
+  defp construct_id(id) do
     [first, second] = String.split(id, "-")
+    "#{System.system_time(:millisecond)}-#{id}"
+  end
+
+  defp convert_to_id(full_id) do
+    [first, second] = String.split(full_id, "-")
+    first
   end
 end
